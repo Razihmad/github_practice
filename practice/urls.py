35c63c4732cd3c11b4e16e137d98ae08ade3b933
RAZI AHMAD <60400893+Razihmad@users.noreply.github.com>
2021-12-15 12:28:48 +0530

    changes

--- practice/urls.py
+++ practice/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('ra/',views.ra,name='ra'),
+    path('testing/',views.testing),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
